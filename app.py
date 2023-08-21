757e3c18a6e62f83a76884b156f2bedd9c49c898
조윤서 <124692313+chobbong@users.noreply.github.com>
2023-08-21 15:58:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class RealEstateDB:
 
      def connect(self):
           # MariaDB 연결
-          self.conn = pymysql.connect(host='khrpa.com', user='joyunseo77', password='whgkdbsWhdbstj77', charset='utf8', database='joyunseo77')
+          self.conn = pymysql.connect(host='khrpa.com', user='joyunseo77', password='', charset='utf8', database='j7')
           self.cursor = self.conn.cursor()
 
      def is_connected(self):
